556362dec9d06bcf6e80e332fe376b9181ce39a7
rcgoff <rcgoff@protonmail.ch>
2021-09-13 19:32:18 +0500

    MS-DOS build script with debug2bin file auto-generation

--- build-dos/build.bat
+++ build-dos/build.bat
@@ -1,8 +1,17 @@
-rem Build with MS-DOS toolchain (up to win7). L.Yadrennikov 21.04.2020
+@echo off
+rem Build with MS-DOS toolchain (up to win7). Version 2. L.Yadrennikov 29.09.2020
 del %1.bin
 masm %1,%1.obj,%1.lst,nul
-link %1,BIOS-ISO.EXE,%1.map,,
+link %1,%1.EXE,%1.map,,
+echo n%1.exe>debug2bin
+echo l>>debug2bin
+echo rcx>>debug2bin
+echo 4000>>debug2bin
+echo n%1.bin>>debug2bin
+echo ecs:fffe fe 20>>debug2bin
+echo wcs:c000>>debug2bin
+echo q>>debug2bin
 debug<debug2bin
-ren bios.bin %1.bin
+
 
 
