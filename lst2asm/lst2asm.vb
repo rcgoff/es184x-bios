5e52d07bccc8bb7208b244b60820cdb3ce14a878
rcgoff <rcgoff@protonmail.ch>
2021-10-07 23:26:56 +0500

    lst2asm:TASM5 format, dup-deletion modified, CR-LF correction added

--- lst2asm/lst2asm.vb
+++ lst2asm/lst2asm.vb
@@ -3,11 +3,18 @@ Option Explicit
 'преобразование файла листинга MASM 3.0 в файл .asm
 'написано в рамках переделки BIOS ЕС1841
 'Л.Ядренников 21,25.04.2020
-'05.04.2021 - изменения для обработки листинга без нумерации строк (необходимо переключить переменную isPageNumbersInFile);
+'05.04.2021 - изменения для обработки листинга без нумерации строк (необходимо переключить переменную isLineNumbersInFile);
 '             также сделана обработка обрезаемых строк, оказавшихся на границе страниц
-'26.09.2021 - удаление лишней пустой строки, добавляемой ассемблером в листинг в конце dup-последовательностей;
+'26.09.2021 - удаление лишней пустой строки, добавляемой ассемблером MASM3 в листинг в конце dup-последовательностей;
 '             проверка на затирание выходных файлов, если они уже есть
+'07.10.2021 - обработка нескольких разных форматов (необходимо изменить строковую переменную LstFormat);
+'             добавлен формат TASM 5.0 (по файлам Глеба Ларионова из Праги);
+'             исправлена ошибка, если в конце файла перевод страницы и нужного числа пустых строк не набирается;
+'             добавлено исправление CR-LF
+'             доработано удаление пустых строк после dup(?) - и для db, и для dw
 'ЕЩЕ НЕ РЕАЛИЗОВАНО: рассовывание включенных по include файлов в разные asm-файлы
+'ЕЩЕ НЕ РЕАЛИЗОВАНО: ListBox выбора формата и вообще интерфейс
+'ЕЩЕ НЕ РЕАЛИЗОВАНО: сохранение файлов с LF вместо CR-LF под другим именем (а не перезапись)
 
 Sub InPath()  'процедура основная, выбор файлов
 Dim flname As Variant           'это потому что по-другому не обрабатываются collection
@@ -28,7 +35,7 @@ Dim lstline As String
 Dim prevline As String
 Dim i As Integer
 Dim FirstCharCode As String
-Dim lstcnt As Integer
+Dim lstcnt As Long
 
 Dim stoplisting As Integer
 
@@ -38,49 +45,97 @@ Dim longlineCnt As Integer
 Dim newpageCnt As Integer
 Dim asmString As String
 Dim codeSection As String
+Dim PrevLineEndDup As Boolean       'предыдущая строка - последняя в DUP (закр. квадр. скобка)
+Dim FileExsistAnswer As Integer     'ответ пользователя, если файл существует
+Dim CRLFrewriteAnswer As Integer    'ответ пользователя - перезаписывать ли файл с неверным CR-LF
 
 'константы формата листинга
-Dim isPageNumbersInFile As Boolean  'начинается ли строка листинга с номера строки или нет
+Dim LstFormat As String             'имя формата листинга
+Dim isLineNumbersInFile As Boolean  'начинается ли строка листинга с номера строки или нет
 Dim AsmFileDataBegin As Integer     'позиция начала данных, переносимых из asm-файла
 Dim LstAddrBegin As Integer         'позиция начала адреса
 Dim LstMachCodeBegin As Integer     'позиция начала машкода
 Dim LstMachCodeEnd As Integer       'позиция конца машкода
+Dim NextPageSkippedLines As Integer 'сколько строк пропускать при встрече новой страницы (считая ту, которая с ASCII 12)
+Dim EndingPhrase As String          'фраза, после которой в листинге идет таблица символов (ее не включаем в asm)
+Dim EndingFirstSym As String        'первый символ этой фразы (заполняется автоматически)
+Dim EndingPhraseLen As Integer      'длина этой фразы (заполняется автоматически)
 
-Dim PrevLineEndDup As Boolean       'предыдущая строка - последняя в DUP (закр. квадр. скобка)
-Dim FileExsistAnswer As Integer     'ответ пользователя, если файл существует
 
+'здесь нужно раскомментировать нужный формат (только один!)
 '=====================================================================
-isPageNumbersInFile = False
+LstFormat = "MASM3 with line numbers"
+'LstFormat = "MASM3 without line numbers"
+'LstFormat = "TASM5 with line numbers"
 '=====================================================================
 
-If isPageNumbersInFile = True Then
+Select Case LstFormat
+
+Case "MASM3 with line numbers"
         'это параметры листинга ЕС-1841, распространенного в интернете на различных площадках
+    isLineNumbersInFile = True
     AsmFileDataBegin = 41
     LstAddrBegin = 10
     LstMachCodeBegin = 16
     LstMachCodeEnd = 32
-Else
+    NextPageSkippedLines = 4
+    EndingPhrase = "Segments and Groups:"
+
+Case "MASM3 without line numbers"
         'если листинг без номеров страниц, позиции становятся на 8 (одну позицию tab) меньше. Это листинг, генерируемый на моем компе
+    isLineNumbersInFile = False
     AsmFileDataBegin = 33
     LstAddrBegin = 2
     LstMachCodeBegin = 8
     LstMachCodeEnd = 26    'это не на 8 меньше, а посмотрел по реальному файлу до link-овки, только после ассемблера
-End If
+    NextPageSkippedLines = 4
+    EndingPhrase = "Segments and Groups:"
+
+Case "TASM5 with line numbers"
+        'параметры листинга Turbo Assembler 5.0, по которому компилируются файлы Gleb'а из Чехии
+    isLineNumbersInFile = True
+    AsmFileDataBegin = 38
+    LstAddrBegin = 9
+    LstMachCodeBegin = 15
+    LstMachCodeEnd = 34    'там дальше в 35-й позиции "+", но он нам не нужен
+    NextPageSkippedLines = 5
+    EndingPhrase = "Symbol Name"
+End Select
     
 
 'инициализация динамических массивов
 ReDim LongLineArray(1 To 1): LongLineArray(1) = 0
 ReDim NewpageArray(1 To 1): NewpageArray(1) = 0
+lstcnt = 0
 longlineCnt = 1
 newpageCnt = 1
 stoplisting = 0
 PrevLineEndDup = False
 FileExsistAnswer = 0
+EndingFirstSym = Left(EndingPhrase, 1)
+EndingPhraseLen = Len(EndingPhrase)
 
 'первый проход - заполнение массивов
+FirstPass:
 Open flname For Input As #1
 Do While Not EOF(1)
     Line Input #1, lstline
+    If lstcnt = 0 Then
+        If LenB(lstline) > LOF(1) / 2 Then
+        'так бывает, если вместо CR-LF стоят LF, и при чтении первой же строки считывается весь файл
+        'почему-то точного равенства не получается, получается немного меньше
+        'LenB - для корректной обработки UTF-8 файлов, т.к. просто Len выдаст длину примерно вдвое меньше, чем LOF
+            CRLFrewriteAnswer = MsgBox(flname & _
+             ": LST file probably contains wrong (non-Windows) line endings, correct and rewrite the file? ", _
+             vbQuestion + vbYesNo + vbDefaultButton2, "CRLF error")
+            If CRLFrewriteAnswer = vbYes Then
+                Close #1
+                Call CRLF(CStr(flname))
+                GoTo FirstPass
+            Else: GoTo EndProgram
+            End If
+        End If
+    End If
     lstcnt = lstcnt + 1
     If Len(lstline) > 0 Then
         FirstCharCode = Asc(Left(lstline, 1))
@@ -90,12 +145,12 @@ Do While Not EOF(1)
             ReDim Preserve NewpageArray(1 To newpageCnt)
         End If
         If FirstCharCode = 9 And newpageCnt > 1 Then
-        'не считаем три пустые строки после перевода страницы
-            If lstcnt - NewpageArray(newpageCnt - 1) > 3 Then
+        'не считаем пустые строки, оставшиеся после перевода страницы
+            If lstcnt - NewpageArray(newpageCnt - 1) > (NextPageSkippedLines - 1) Then
                 'если строка, следующая сразу за автоматически вставленными с началом страницы, является перенесенной
-                If lstcnt - NewpageArray(newpageCnt - 1) = 4 Then
+                If lstcnt - NewpageArray(newpageCnt - 1) = NextPageSkippedLines Then
                         'то маркируем начало строки, оставшееся на предыдущей странице
-                        LongLineArray(longlineCnt) = lstcnt - 5
+                        LongLineArray(longlineCnt) = lstcnt - (NextPageSkippedLines + 1)
                     Else
                         LongLineArray(longlineCnt) = lstcnt - 1 'иначе маркируем просто предыдущую строку
                     End If
@@ -103,8 +158,8 @@ Do While Not EOF(1)
                 ReDim Preserve LongLineArray(1 To longlineCnt)
             End If
         End If
-        If Chr(FirstCharCode) = "S" Then
-            If Left(lstline, 20) = "Segments and Groups:" Then
+        If Chr(FirstCharCode) = EndingFirstSym Then
+            If Left(lstline, EndingPhraseLen) = EndingPhrase Then
                 stoplisting = lstcnt 'конец зоны, где нужно укорачивать строки
             End If
         End If
@@ -147,11 +202,14 @@ Do While Not EOF(1)
     Line Input #1, lstline
     lstcnt = lstcnt + 1
     If lstcnt = NewpageArray(newpageCnt) Then
-    'при встрече новой страницы вхолостую прочесть 4 строки (уже считанную+3)
+    'при встрече новой страницы вхолостую прочесть NextPageSkippedLines строк (уже считанную+NextPageSkippedLines-1)
     'и еще одну строку - уже новую, непустую - занести в lstline
-        For i = 1 To 4
-            Line Input #1, lstline
-            lstcnt = lstcnt + 1
+        For i = 1 To NextPageSkippedLines
+            If Not EOF(1) Then
+                Line Input #1, lstline
+                lstcnt = lstcnt + 1
+            Else: Exit For          'на тот случай, если в конце файла перевод страницы и нужного числа пустых строк не набирается
+            End If
         Next i
         newpageCnt = newpageCnt + 1
     End If
@@ -203,13 +261,11 @@ Do While Not EOF(1)
             
             'отловим ложную пустую строку, возникающую в конце dup-последовательностей,
             'если после dup в строке кода ничего не было
-            If Len(codeSection) > 3 Then
-                If Mid(codeSection, Len(codeSection) - 3, 1) = "]" Then         'была ли текущая строка третьей в DUP-последовательности?
-                                                                                '(работать с ней будем на следующем проходе)
-                    PrevLineEndDup = True
-                Else
-                    PrevLineEndDup = False
-                End If
+            If StringExceptSpacesTabs(codeSection) = "]" Then        'была ли текущая строка третьей в DUP-последовательности?
+                                                                     '(работать с ней будем на следующем проходе)
+                PrevLineEndDup = True
+            Else
+                PrevLineEndDup = False
             End If
         End If
         Print #3, lstline
@@ -317,12 +373,6 @@ MsgBox (isStringEmpty(""))
 MsgBox (isStringEmpty("  v v"))
 End Sub
 
-Sub testget()
-'тест корявый - при copy-paste сюда табуляция замещается пробелами
-MsgBox (GetShortLine("9688                  ;   Переход по включению питания"))
-End Sub
-
-
 
 'объявление собственного типа для создания функции, которая возвращает два значения
 'больше не используется
@@ -335,3 +385,28 @@ End Sub
 'End Type
 
 
+ Sub CRLF(MyFile As String)
+ 'расстановка CR-LF для корректной работы текстовых процедур
+ '(c)rcgoff 2009
+  
+ Dim tempstr As String
+Dim tempstr2 As String
+ Open MyFile For Input As #1
+ Line Input #1, tempstr
+ 'If LOF(1) <> Len(tempstr) Then Close #1: Exit Sub      'если длины не равны, значит корректно считалась одна строка и файл нормальный
+ tempstr2 = Replace(tempstr, Chr(10), Chr(13) + Chr(10))
+ Close #1
+ Kill MyFile
+ Open MyFile For Output As #1
+ Print #1, tempstr2
+ Close #1
+ End Sub
+
+Function StringExceptSpacesTabs(currStr As String) As String
+'возвращает строку без пробелов и табуляций
+
+    currStr = Replace(currStr, " ", "")
+    currStr = Replace(currStr, vbTab, "")
+ 
+StringExceptSpacesTabs = currStr
+End Function
